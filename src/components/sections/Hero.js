9ba97c7bd03ddc6470390c5de6fe47656de9157a
FeedoneH <fidanhasanguliyeva@gmail.com>
2020-12-21 17:05:41 +0400

    final commit

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -60,16 +60,17 @@ const Hero = ({
               className="mt-0 mb-16 reveal-from-bottom"
               data-reveal-delay="200"
             >
-              Landing template for{" "}
-              <span className="text-color-primary">startups</span>
+              Lorem Ipsum dolor{" "}
+              <span className="text-color-primary">sit amet</span>
             </h1>
             <div className="container-xs">
               <p
                 className="m-0 mb-32 reveal-from-bottom"
                 data-reveal-delay="400"
               >
-                Our landing page template works on all devices, so you only have
-                to set it up once, and get beautiful results forever.
+                Proin tempus interdum odio, sit amet ornare est pretium sit
+                amet. Donec sed massa ac nulla condimentum hendrerit id id enim.
+                Duis sed faucibus est.{" "}
               </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
